0434c3c2a78a98b731f8434fca19d252b415f3b0
munavirt <munavirt79@gmail.com>
2023-10-29 02:25:23 +0530

    fixing aws deploy error first time

--- addon/settings.py
+++ addon/settings.py
@@ -90,11 +90,11 @@ AUTH_USER_MODEL = 'Accounts.Account'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'addon_db',
-        'USER': 'addon',
-        'PASSWORD': 'addon.info%',
-        'HOST':'addon-db.cdrquefr4z7x.ap-south-1.rds.amazonaws.com',
-        'PORT':'5432'
+        'NAME': 'test',
+        'USER': 'test',
+        'PASSWORD': 'test1234',
+        'HOST': 'test.cd1ho58a6nmb.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
@@ -148,3 +148,12 @@ MEDIA_URL = 'media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+AWS_ACCESS_KEY_ID = 'AKIAQGDGLQ2ZWCKRDVVZ'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'testaddon'
+AWS_S3_SIGNATURE_NAME = 's3v4',
+AWS_S3_REGION_NAME = 'us-east-1'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL =  None
+AWS_S3_VERITY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
