8c7d75cd58b498deefbf737b53cc3925f8a11025
munavirt <munavirt79@gmail.com>
2023-10-29 17:59:53 +0530

    sqlite for test purpose

--- addon/settings.py
+++ addon/settings.py
@@ -82,23 +82,23 @@ AUTH_USER_MODEL = 'Accounts.Account'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'test',
-        'USER': 'test',
-        'PASSWORD': 'test1234',
-        'HOST': 'test.cd1ho58a6nmb.us-east-1.rds.amazonaws.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'test',
+#         'USER': 'test',
+#         'PASSWORD': 'test1234',
+#         'HOST': 'test.cd1ho58a6nmb.us-east-1.rds.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
@@ -150,12 +150,12 @@ MEDIA_URL = 'media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-AWS_ACCESS_KEY_ID = 'AKIAQGDGLQ2ZWCKRDVVZ'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'testaddon'
-AWS_S3_SIGNATURE_NAME = 's3v4',
-AWS_S3_REGION_NAME = 'us-east-1'
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL =  None
-AWS_S3_VERITY = True
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# AWS_ACCESS_KEY_ID = 'AKIAQGDGLQ2ZWCKRDVVZ'
+# AWS_SECRET_ACCESS_KEY = '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'
+# AWS_STORAGE_BUCKET_NAME = 'testaddon'
+# AWS_S3_SIGNATURE_NAME = 's3v4',
+# AWS_S3_REGION_NAME = 'us-east-1'
+# AWS_S3_FILE_OVERWRITE = False
+# AWS_DEFAULT_ACL =  None
+# AWS_S3_VERITY = True
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
